f78ac1544f3a843f45cde15c3dccf6a9e3f84471
ajakobso <ajakobso@g.jct.ac.il>
2021-10-04 23:09:47 +0300

    Initial code + Welcome functions

---
+++ dotNet5782_4020_7998/Targil0/Program4020.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4020();
+            Welcome7998();
+
+        }
+        static partial void Welcome7998();
+        private static void Welcome4020()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
