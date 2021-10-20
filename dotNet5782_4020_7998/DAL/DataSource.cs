5f2d47c4f13a3fff2f1fb1a287c282f2ab6eba3f
ajakobso <ajakobso@g.jct.ac.il>
2021-10-20 14:27:22 +0300

    List#1

--- dotNet5782_4020_7998/DAL/DataSource.cs
+++ dotNet5782_4020_7998/DAL/DataSource.cs
@@ -8,8 +8,8 @@ namespace DalObject
 {
     class DataSource
     {
-        internal Drone[] Drones = new Drone[10];
-        internal BaseStation[] BaseStations = new BaseStation[5];
+        internal List<Drone> Drones = new List<Drone>();
+        internal List<BaseStation> BaseStations = new List<BaseStation>();
         internal Customer[] Customers = new Customer[100];
         internal Parcel[] Parcels = new Parcel[1000];
        internal class Config//fields of the first empty element in every array.
@@ -23,7 +23,13 @@ namespace DalObject
         //מתודה סטטית  לאיתחול מופעי הישויות עם נתונים וכו
         static void Initialize ()
         {
-            ////////////////////////////////
+            Random r = new Random();
+            BaseStations[0].Name = "station1";
+            for (int i = 0; i < 10; i++)
+            {
+
+
+            }
         }
     }
 }
