0d841046936ef259ef46d5d56e46406004366f75
etyali <etya@g.jct.ac.il>
2021-10-25 16:02:59 +0300

    add function in the namespace "DalObject"

--- dotNet5782_4020_7998/DAL/DalObject.cs
+++ dotNet5782_4020_7998/DAL/DalObject.cs
@@ -11,25 +11,25 @@ namespace DalObject
     {
         public static void AddBaseStation(int id, string name, int chargeSlots, double longitude, double lattitude)
         {
-            DataSource.BaseStations.Add(new BaseStation { Id = id, Name = name, ChargeSlots = chargeSlots, Longitude = longitude, Lattitude = lattitude });
+            DataSource.Config.BaseStations.Add(new BaseStation { Id = id, Name = name, ChargeSlots = chargeSlots, Longitude = longitude, Lattitude = lattitude });
         }
         public static void AddDrone(int id, double battery, WeightCategories maxW, string model, DroneStatuses status)
         {
-            DataSource.Drones.Add(new Drone { Id = id, Battery = battery, MaxWeight = maxW, Model = model, Status = status });
+            DataSource.Config.Drones.Add(new Drone { Id = id, Battery = battery, MaxWeight = maxW, Model = model, Status = status });
         }
         public static void AddCustomer(int id, string name, string phone, double longitude, double lattitude)
         {
-            DataSource.Customers.Add(new Customer { Id = id, Name = name, Phone = phone, Longitude = longitude, Lattitude = lattitude });
+            DataSource.Config.Customers.Add(new Customer { Id = id, Name = name, Phone = phone, Longitude = longitude, Lattitude = lattitude });
         }
         public static void AddParcel(int droneId, int senderId, int targetId, Priorities priority, WeightCategories weight, DateTime requested, DateTime scheduled, DateTime pickedUp, DateTime delivered)
         {
-            DataSource.Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = droneId, SenderId = senderId, TargetId = targetId, Priority = priority, Weight = weight, Requested = requested, Scheduleded = scheduled, PickedUp = pickedUp, Delivered = delivered });
+            DataSource.Config.Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = droneId, SenderId = senderId, TargetId = targetId, Priority = priority, Weight = weight, Requested = requested, Scheduleded = scheduled, PickedUp = pickedUp, Delivered = delivered });
         }
         public static void AscriptionPtoD(Parcel parcel)// ascription a parcel with an available drone
         {
-            foreach (Drone drone in DataSource.Drones)
+            foreach (Drone drone in DataSource.Config.Drones)
             {
-                if(drone.Status==DroneStatuses.Available)
+                if (drone.Status == DroneStatuses.Available)
                 {
                     parcel.DroneId = drone.Id;
                     return;
@@ -38,40 +38,163 @@ namespace DalObject
         }
         public static void PickUpParcel(Parcel parcel)
         {
-            
-            foreach (Drone drone in DataSource.Drones)
+
+            foreach (Drone drone in DataSource.Config.Drones)
             {
                 if (drone.Id == parcel.DroneId)
                 {
                     Drone newDrone = new Drone { Id = drone.Id, Status = DroneStatuses.Shipping, Battery = drone.Battery, MaxWeight = drone.MaxWeight, Model = drone.Model };
-                    DataSource.Drones.Remove(drone);
-                    DataSource.Drones.Add(newDrone);
+                    DataSource.Config.Drones.Remove(drone);
+                    DataSource.Config.Drones.Add(newDrone);
                     return;
                 }
             }
         }
         public static void PaclerDelivering(int parcelId)//אם קלט הפונקציה זה איבר מסוג חבילה אז אפשר למחוק את הלולאה של פוראיצ הראשונה, העיקרון שעשיתי פה אבל ישמש אותנו בפונקציות של ההצגה של איבר/רשימה.
         {
-            foreach (Parcel parcel in DataSource.Parcels)///find the pacler by its id
+            foreach (Parcel parcel in DataSource.Config.Parcels)///find the pacler by its id
             {
-                if(parcel.Id == parcelId)
+                if (parcel.Id == parcelId)
                 {
-                    foreach (Drone drone in DataSource.Drones)///find the drone that ascribed to the pacler
+                    foreach (Drone drone in DataSource.Config.Drones)///find the drone that ascribed to the pacler
                     {
                         if (drone.Id == parcel.DroneId)
                         {
                             Drone newDrone = new Drone { Id = drone.Id, Status = DroneStatuses.Available, Battery = drone.Battery, MaxWeight = drone.MaxWeight, Model = drone.Model };
-                            DataSource.Drones.Remove(drone);
-                            DataSource.Drones.Add(newDrone);///change the status of the drone into available because he finish the shipment.
+                            DataSource.Config.Drones.Remove(drone);
+                            DataSource.Config.Drones.Add(newDrone);///change the status of the drone into available because he finish the shipment.
                             Parcel newParcel = new Parcel { Id = parcel.Id, Delivered = DateTime.Now, DroneId = parcel.DroneId, PickedUp = parcel.PickedUp, Priority = parcel.Priority, Requested = parcel.Requested, Scheduleded = parcel.Scheduleded, SenderId = parcel.SenderId, TargetId = parcel.TargetId, Weight = parcel.Weight };
-                            DataSource.Parcels.Remove(parcel);
-                            DataSource.Parcels.Add(newParcel);//updating the delivering time of the parcel
+                            DataSource.Config.Parcels.Remove(parcel);
+                            DataSource.Config.Parcels.Add(newParcel);//updating the delivering time of the parcel
                             return;
                         }
                     }
                 }
             }
         }
+        public static void DroneCharging(int droneId, int baseStationId)//inserting a drone into a charging station in order to charge his battery
+        {
+            foreach (Drone drone in DataSource.Config.Drones)
+            {
+                if (drone.Id == droneId)
+                {
+                    Drone newDrone = new Drone { Id = drone.Id, Status = DroneStatuses.Maintenance, Battery = drone.Battery, MaxWeight = drone.MaxWeight, Model = drone.Model };
+                    DataSource.Config.Drones.Remove(drone);
+                    DataSource.Config.Drones.Add(newDrone); ///change the status of the drone into maintenance because he need to charge.
+                    DroneCharge newDCharge = new DroneCharge { DroneId = droneId, StationId = baseStationId }; ///
+                }
+
+            }
+        }
+        public static void DroneRelease(int droneId, int baseStationId)//Release the drone from the charging station                                                                 
+        {
+            foreach (Drone drone in DataSource.Config.Drones)
+            {
+                if (drone.Id == droneId)
+                {
+                    Drone newDrone = new Drone { Id = drone.Id, Status = DroneStatuses.Available, Battery = drone.Battery, MaxWeight = drone.MaxWeight, Model = drone.Model };
+                    DataSource.Config.Drones.Remove(drone);
+                    DataSource.Config.Drones.Add(newDrone); ///change the status of the drone into available because the user's request
+                    foreach (DroneCharge charger in DataSource.Config.DroneCharges)///remove the matching charging station from the list
+                    {
+                        if (charger.DroneId == droneId && charger.StationId == baseStationId)
+                        {
+                            DataSource.Config.DroneCharges.Remove(charger);
+                        }
+                    }
+                }
+
+            }
+        }
+        public static BaseStation CopyBaseStation(int baseStationId)//return copy of a base station
+        {
+            BaseStation nBStation = new BaseStation();
+            foreach (BaseStation baseStation in DataSource.Config.BaseStations)
+            {
+                if (baseStation.Id == baseStationId)
+                {
+                    return baseStation;
+                }
+            }
+            return nBStation;//the function demend us to return a value, and because the return is inside a condition it cause an error
+        }
+        public static Drone CopyDrone(int droneId)//return copy of a drone
+        {
+            Drone nDrone = new Drone();
+            foreach (Drone drone in DataSource.Config.Drones)
+            {
+                if (drone.Id == droneId)
+                {
+                    return drone;
+                }
+            }
+            return nDrone;//the function demend us to return a value, and because the return is inside a condition it cause an error
+        }
+        public static Customer CopyCustomer(int customerId)//return copy of a customer
+        {
+            Customer nCustomer = new Customer();
+            foreach (Customer customer in DataSource.Config.Customers)
+            {
+                if (customer.Id == customerId)
+                {
+                    return customer;
+                }
+            }
+            return nCustomer;//the function demend us to return a value, and because the return is inside a condition it cause an error
+        }
+        public static Parcel CopyParcel(int parcelId)//return copy of a parcel
+        {
+            Parcel nParcel = new Parcel();
+            foreach (Parcel parcel in DataSource.Config.Parcels)
+            {
+                if (parcel.Id == parcelId)
+                {
+                    return parcel;
+                }
+            }
+            return nParcel;//the function demend us to return a value, and because the return is inside a condition it cause an error
+        }
+        public static List<BaseStation> CopyBaseStations()//return copy of the base stations's list
+        {
+            return DataSource.Config.BaseStations;
+        }
+        public static List<Drone> CopyDronesList()//return copy of the drones's list
+        {
+            return DataSource.Config.Drones;
+        }
+        public static List<Customer> CopyCustomersList()//return copy of the customer's list
+        {
+            return DataSource.Config.Customers;
+        }
+        public static List<Parcel> CopyParcelsList()//return copy of the parcels's list
+        {
+            return DataSource.Config.Parcels;
+        }
+        public static List<Parcel> UnAscriptedParcels()//return new list with all the un-ascripted parcels.
+        {
+            List<Parcel> nList = new List<Parcel>();
+            foreach (Parcel parcel in DataSource.Config.Parcels)
+            {
+                if (parcel.DroneId == 0)
+                {
+                    nList.Add(parcel);
+                }
+            }
+            return nList;
+        }
+        public static List<BaseStation> AvailableBaseStation()//return new list with the base stations who have available charge slots.
+        {
+            List<BaseStation> nList = new List<BaseStation>();
+            foreach(BaseStation baseStation in DataSource.Config.BaseStations)
+            {
+                if (baseStation.ChargeSlots>0)
+                {
+                    nList.Add(baseStation);
+                }
+            }
+            return nList;
+        }
+
 
     }
 }
--- dotNet5782_4020_7998/DAL/DataSource.cs
+++ dotNet5782_4020_7998/DAL/DataSource.cs
@@ -4,59 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using IDAL.DO;
+using DalObject;
 namespace DalObject
 {
     class DataSource
     {
-       internal class Config//fields of the first empty element in every array.
+        internal class Config//fields of the first empty element in every array.
         {
-            internal static int FstDroneAvailable = 0;///
-            internal static int FstBaseStationAvailable = 0;
-            internal static int FstCustomerAvailable = 0;
-            internal static int FstParcelavailable = 0;
-            //שדה ליצירת מספר מזהה עבור חבילות
-        internal static List<Drone> Drones = new List<Drone>();
-        internal static List<BaseStation> BaseStations = new List<BaseStation>();
-        internal static List<Customer> Customers = new List<Customer>();
-        internal static List<Parcel> Parcels = new List<Parcel>();
-       internal class Config
-        {
+            internal static List<Drone> Drones = new List<Drone>();
+            internal static List<BaseStation> BaseStations = new List<BaseStation>();
+            internal static List<Customer> Customers = new List<Customer>();
+            internal static List<Parcel> Parcels = new List<Parcel>();
+            internal static List<DroneCharge> DroneCharges = new List<DroneCharge>();
+
+
             internal static int RunningParcelId = 1001;//running number for the parcels id
-            internal static int RunningBSId = 101;//running number for the base stations id
         }
         //מתודה סטטית  לאיתחול מופעי הישויות עם נתונים וכו
-        static void Initialize ()// לבדוק אם באמת אפשר את הרחפנים ואת התחנות בסיס להגדיר עם מספר רץ
+        public static void Initialize()
         {
             Random r = new Random();
-            BaseStations.Add(new BaseStation { Id = DataSource.Config.RunningBSId++, Name = "station1", ChargeSlots = 3, Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });//initializing stations around Jerusalem
-            BaseStations.Add(new BaseStation { Id = DataSource.Config.RunningBSId++, Name = "station2", ChargeSlots = 5, Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
-            Drones.Add(new Drone { Id = 1, Battery = r.NextDouble() + r.Next(1, 100), MaxWeight = WeightCategories.Light, Model = "10A", Status = DroneStatuses.Maintenance });
-            Drones.Add(new Drone { Id = 2, Battery = r.NextDouble() + r.Next(1, 100), MaxWeight = WeightCategories.Heavy, Model = "5A", Status = DroneStatuses.Available });
-            Drones.Add(new Drone { Id = 3, Battery = r.NextDouble() + r.Next(1, 100), MaxWeight = WeightCategories.Middle, Model = "10B", Status = DroneStatuses.Available });
-            Drones.Add(new Drone { Id = 4, Battery = r.NextDouble() + r.Next(1, 100), MaxWeight = WeightCategories.Heavy, Model = "15C", Status = DroneStatuses.Shipping });
-            Drones.Add(new Drone { Id = 5, Battery = r.NextDouble() + r.Next(1, 100), MaxWeight = WeightCategories.Heavy, Model = "10D", Status = DroneStatuses.Shipping });
-            Drones.Add(new Drone { Id = 6, Battery = r.NextDouble() + r.Next(1, 100), MaxWeight = WeightCategories.Heavy, Model = "15D", Status = DroneStatuses.Shipping });
-            Customers.Add(new Customer { Id = 326456189, Name = "Avital", Phone = "0548651821", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
-            Customers.Add(new Customer { Id = 204168946, Name = "Avi", Phone = "0548679821", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
-            Customers.Add(new Customer { Id = 425891358, Name = "Lulu", Phone = "0548768521", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
-            Customers.Add(new Customer { Id = 702594863, Name = "Talya", Phone = "0548619897", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
-            Customers.Add(new Customer { Id = 203459782, Name = "Phoebe", Phone = "0548679846", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
-            Customers.Add(new Customer { Id = 306894751, Name = "Moti", Phone = "0548489651", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
-            Customers.Add(new Customer { Id = 451620785, Name = "Ben", Phone = "0548498147", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
-            Customers.Add(new Customer { Id = 410258943, Name = "Nati", Phone = "0589756121", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
-            Customers.Add(new Customer { Id = 327498510, Name = "Batty", Phone = "0542235829", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
-            Customers.Add(new Customer { Id = 610845302, Name = "Rachel", Phone = "0548645679", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
-            Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 1, SenderId = 425891358, TargetId = 326456189, Priority = Priorities.Fast, Weight = WeightCategories.Heavy, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
-            Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 5, SenderId = 425891358, TargetId = 610845302, Priority = Priorities.Standart, Weight = WeightCategories.Heavy, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
-            Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 3, SenderId = 610845302, TargetId = 326456189, Priority = Priorities.Fast, Weight = WeightCategories.Middle, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
-            Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 2, SenderId = 425891358, TargetId = 203459782, Priority = Priorities.Urgent, Weight = WeightCategories.Heavy, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
-            Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 6, SenderId = 203459782, TargetId = 425891358, Priority = Priorities.Fast, Weight = WeightCategories.Heavy, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
-            Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 3, SenderId = 425891358, TargetId = 326456189, Priority = Priorities.Standart, Weight = WeightCategories.Middle, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
-            Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 4, SenderId = 204168946, TargetId = 327498510, Priority = Priorities.Fast, Weight = WeightCategories.Heavy, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
-            Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 1, SenderId = 702594863, TargetId = 326456189, Priority = Priorities.Standart, Weight = WeightCategories.Middle, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
-            Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 5, SenderId = 425891358, TargetId = 204168946, Priority = Priorities.Standart, Weight = WeightCategories.Heavy, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
-            Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 2, SenderId = 327498510, TargetId = 702594863, Priority = Priorities.Standart, Weight = WeightCategories.Middle, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
-
+            Config.BaseStations.Add(new BaseStation { Id = 101, Name = "station1", ChargeSlots = 3, Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });//initializing stations around Jerusalem
+            Config.BaseStations.Add(new BaseStation { Id = 102, Name = "station2", ChargeSlots = 5, Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
+            Config.Drones.Add(new Drone { Id = 1, Battery = r.NextDouble() + r.Next(1, 100), MaxWeight = WeightCategories.Light, Model = "10A", Status = DroneStatuses.Maintenance });
+            Config.Drones.Add(new Drone { Id = 2, Battery = r.NextDouble() + r.Next(1, 100), MaxWeight = WeightCategories.Heavy, Model = "5A", Status = DroneStatuses.Available });
+            Config.Drones.Add(new Drone { Id = 3, Battery = r.NextDouble() + r.Next(1, 100), MaxWeight = WeightCategories.Middle, Model = "10B", Status = DroneStatuses.Available });
+            Config.Drones.Add(new Drone { Id = 4, Battery = r.NextDouble() + r.Next(1, 100), MaxWeight = WeightCategories.Heavy, Model = "15C", Status = DroneStatuses.Shipping });
+            Config.Drones.Add(new Drone { Id = 5, Battery = r.NextDouble() + r.Next(1, 100), MaxWeight = WeightCategories.Heavy, Model = "10D", Status = DroneStatuses.Shipping });
+            Config.Drones.Add(new Drone { Id = 6, Battery = r.NextDouble() + r.Next(1, 100), MaxWeight = WeightCategories.Heavy, Model = "15D", Status = DroneStatuses.Shipping });
+            Config.Customers.Add(new Customer { Id = 326456189, Name = "Avital", Phone = "0548651821", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
+            Config.Customers.Add(new Customer { Id = 204168946, Name = "Yosi", Phone = "0548679821", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
+            Config.Customers.Add(new Customer { Id = 425891358, Name = "Lulu", Phone = "0548768521", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
+            Config.Customers.Add(new Customer { Id = 702594863, Name = "Talya", Phone = "0548619897", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
+            Config.Customers.Add(new Customer { Id = 203459782, Name = "Monica", Phone = "0548679846", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
+            Config.Customers.Add(new Customer { Id = 306894751, Name = "Moti", Phone = "0548489651", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
+            Config.Customers.Add(new Customer { Id = 451620785, Name = "Ben", Phone = "0548498147", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
+            Config.Customers.Add(new Customer { Id = 410258943, Name = "Nati", Phone = "0589756121", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
+            Config.Customers.Add(new Customer { Id = 327498510, Name = "Ayelet", Phone = "0542235829", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
+            Config.Customers.Add(new Customer { Id = 610845302, Name = "Rachel", Phone = "0548645679", Longitude = 31 + r.NextDouble(), Lattitude = 35 + r.NextDouble() });
+            Config.Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 0, SenderId = 425891358, TargetId = 326456189, Priority = Priorities.Fast, Weight = WeightCategories.Heavy, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
+            Config.Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 5, SenderId = 425891358, TargetId = 610845302, Priority = Priorities.Standart, Weight = WeightCategories.Heavy, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
+            Config.Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 3, SenderId = 610845302, TargetId = 326456189, Priority = Priorities.Fast, Weight = WeightCategories.Middle, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
+            Config.Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 0, SenderId = 425891358, TargetId = 203459782, Priority = Priorities.Urgent, Weight = WeightCategories.Heavy, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
+            Config.Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 6, SenderId = 203459782, TargetId = 425891358, Priority = Priorities.Fast, Weight = WeightCategories.Heavy, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
+            Config.Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 0, SenderId = 425891358, TargetId = 326456189, Priority = Priorities.Standart, Weight = WeightCategories.Middle, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
+            Config.Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 4, SenderId = 204168946, TargetId = 327498510, Priority = Priorities.Fast, Weight = WeightCategories.Heavy, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
+            Config.Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 1, SenderId = 702594863, TargetId = 326456189, Priority = Priorities.Standart, Weight = WeightCategories.Middle, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
+            Config.Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 0, SenderId = 425891358, TargetId = 204168946, Priority = Priorities.Standart, Weight = WeightCategories.Heavy, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
+            Config.Parcels.Add(new Parcel { Id = DataSource.Config.RunningParcelId++, DroneId = 2, SenderId = 327498510, TargetId = 702594863, Priority = Priorities.Standart, Weight = WeightCategories.Middle, Requested = DateTime.Now, Scheduleded = DateTime.Now, PickedUp = DateTime.Now, Delivered = new DateTime(2021, 10, 22) });
+            Config.DroneCharges.Add(new DroneCharge { DroneId = 1, StationId=102 });
         }
     }
 }
