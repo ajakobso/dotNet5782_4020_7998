6af751812fa27bc54ba386535554d77f7e642485
etyali <etya@g.jct.ac.il>
2021-11-25 19:44:14 +0200

    starting interface /ibl

--- BL/IBL.cs
+++ BL/IBL.cs
@@ -8,5 +8,29 @@ namespace IBL.BO
 {
     interface IBL
     {
+        public:
+            void AddBaseStation(int num, string name, string location, int numOfAvailableDCharge);
+            void AddDrone(int Id, char Model, WeightCategories MaxWeight, int Bstation);
+            void AddCustomer(int Id, string Name, int PhoneNum, string Location);
+            void AddParcelToDeliver(SCustomerId, SCustomerId, WeightCategories Weight, Priorities Priority);
+            void UpdateDrone(int Id, string Model);
+            void UpdateBaseStation(int Id, string Name, int NumOfChargeSlots);
+            void UpdateCustomer(int Id, string Name, int PhoneNum);
+            void DroneToCharge(int Id);
+            void ReleaseDroneFromCharge(int Id, DateTime TimeInCharge);
+            void AscriptionParcelToDrone(int Id);
+            void PickUpParcel(int DId);
+            void DeliveringParcelByDrone(int Id);
+            void DisplayBaseStation();
+            void DisplayDrone();
+            void DisplayCustomer();
+            void DisplayParcel();
+            void DisplayBaseStationsList();
+            void DisplayDronesList();
+            void DisplayCustomersList();
+            void DisplayParcelsList();
+            void DisplayUnAscriptedParcelsList();
+            void DisplayAvailableChargingStation();
+
     }
 }
