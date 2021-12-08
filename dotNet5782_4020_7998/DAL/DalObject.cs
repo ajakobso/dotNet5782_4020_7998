fc0c146c5e401810b2b25b5d4eb95812b2121d5a
etyali <etya@g.jct.ac.il>
2021-12-08 20:37:34 +0200

    :)

--- dotNet5782_4020_7998/DAL/DalObject.cs
+++ dotNet5782_4020_7998/DAL/DalObject.cs
@@ -25,7 +25,6 @@ namespace DalObject//add exception of id that didnt found
                 {
                     throw new AddExistingBaseStationException();
                 }
-                if(())
             }
 
             DataSource.Config.BaseStations.Add(new BaseStation { Id = id, Name = name, ChargeSlots = chargeSlots, Longitude = longitude, Lattitude = lattitude });
