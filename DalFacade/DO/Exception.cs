1627b4a04a08be511dc7864d24db5537f2e48948
ajakobso <ajakobso@g.jct.ac.il>
2022-01-02 02:04:45 +0200

    dalXml - done

--- DalFacade/DO/Exception.cs
+++ DalFacade/DO/Exception.cs
@@ -66,4 +66,18 @@ namespace DO
     {
         public DalTypeCantBeProducedException() { }
     }
+
+    [Serializable]
+    public class XmlFileLoadCreateException :Exception
+    {
+        public string xmlFilePath;
+        public XmlFileLoadCreateException(string xmlPath) : base() { xmlFilePath = xmlPath; }
+        public XmlFileLoadCreateException(string xmlPath, string message) :
+            base(message)
+        { xmlFilePath = xmlPath; }
+        public XmlFileLoadCreateException(string xmlPath, string message, Exception innerException) :
+            base(message, innerException)
+        { xmlFilePath = xmlPath; }
+        public override string ToString() => base.ToString() + $", fail to load or create xml file: {xmlFilePath}";
+    }
 }
